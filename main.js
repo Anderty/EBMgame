9f822262661c4d6e58752fd87c75d3dea8a36473
Anderty <anmeyro.org@gmail.com>
2016-09-22 16:29:00 +0300

    Added Polymer Counter
    Some polishings, added space for future tools.

--- main.js
+++ main.js
@@ -1,48 +1,65 @@
-var entities = ['minerals','cursors','cursorCost','polymers', 'polymersProgress',];
+
+/*				Variables				*/
+
+var entities = [
+	"clicks",
+	"minerals",
+	"cursors",
+	"cursorCost",
+	"polymers",
+	"polymersProgress",
+];
+
+var displayData = [
+	"minerals",
+	"cursors",
+	"cursorCost",
+	"polymers",
+];
 
 var data = {
-	'minerals' : 0,
-	'cursors' : 0,
-	'cursorCost' : 10,
-	'polymers' : 0,
-	'polymersProgress' : 0
+	"clicks" : 0,
+	"minerals" : 0,
+	"cursors" : 0,
+	"cursorCost" : 10,
+	"polymers" : 0,
+	"polymersProgress" : 0
 };
 
+/*				Functions				*/
+
 function mineralClick(){
-	data['minerals'] = data['minerals'] + data['cursors'] + 1;
-	document.getElementById("minerals").innerHTML = data['minerals'];
+	data["minerals"] = data["minerals"] + data["cursors"] + 1;
+	data["clicks"] += 1;
+	if (data["polymersProgress"] >= 9 + data["polymers"]) {
+		data["polymers"] += 1;
+		data["polymersProgress"] = 0;
+		document.getElementById("polymers").innerHTML = data["polymers"];
+		//console.log("Polymer Synthesized");
+	} else {
+		data["polymersProgress"] += 1;
+	};
+	document.getElementById("minerals").innerHTML = data["minerals"];
+	//console.log(data["polymersProgress"]);
 };
 
 function autoClick(){
-	data['minerals'] = data['minerals'] + data['cursors'];
-	document.getElementById("minerals").innerHTML = data['minerals'];
+	data["minerals"] = data["minerals"] + data["cursors"];
+	document.getElementById("minerals").innerHTML = data["minerals"];
 };
 
-/*function polymersProgress(){
-	if(data['polymersProgress'] >= 10){
-		console.log('Polymers Reset');
-		data['polymersProgress'] = 0;
-		document.getElementById('polymers').innerHTML = data['polymers'];
-		document.getElementsByClassName('ppactive').className = 'pp';
-	} else {
-		data['polymers'] = data['polymers'] + 1;
-		console.log('Polymers got');
-		document.getElementById('pp' + data['polymersProgress']).className += 'ppactive';
-	};
-};*/
-
 function curCursorCost(){
-	var nextCost = Math.floor(10 * Math.pow(1.1,data['cursors']));
-	document.getElementById('cursorCost').innerHTML = prettify(nextCost);
+	var nextCost = Math.floor(10 * Math.pow(1.1,data["cursors"]));
+	document.getElementById("cursorCost").innerHTML = prettify(nextCost);
 };
 
 function buyCursor(){
-	var cursorCost = Math.floor(10 * Math.pow(1.1,data['cursors']));
-	if(data['minerals'] >= cursorCost){
-		data['cursors'] = data['cursors'] + 1;
-		data['minerals'] = data['minerals'] - cursorCost;
-		document.getElementById('cursors').innerHTML = data['cursors'];
-		document.getElementById('minerals').innerHTML = data['minerals'];
+	var cursorCost = Math.floor(10 * Math.pow(1.1,data["cursors"]));
+	if(data["minerals"] >= cursorCost){
+		data["cursors"] += 1;
+		data["minerals"] = data["minerals"] - cursorCost;
+		document.getElementById("cursors").innerHTML = data["cursors"];
+		document.getElementById("minerals").innerHTML = data["minerals"];
 	};
 	curCursorCost();
 };
@@ -56,20 +73,24 @@ function loadSave(){
 	var saveGame = JSON.parse(localStorage.getItem("ebmSave"));
 	entities.forEach(function(entry){
 		if (typeof saveGame[entry] !== "undefined") data[entry] = saveGame[entry];
+	});
+	displayData.forEach(function(entry){
 		document.getElementById(entry).innerHTML = data[entry];
-		console.log(saveGame[entry] + ' ' + entry + " loaded");
+		console.log(saveGame[entry] + " " + entry + " loaded");
 	});
 	curCursorCost();
 };
 
 function executeSave(){
-	localStorage.setItem("ebmSave",JSON.stringify(data)); 
+	localStorage.setItem("ebmSave",JSON.stringify(data));
 };
 
 function deleteSave(){
 	localStorage.removeItem("ebmSave");
 	window.location.reload(false);
 }
+
+/*				Events				*/
 
 window.onload = function() {
 	loadSave();
